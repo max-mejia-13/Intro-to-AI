3edbcd1c712bdd666da2fb7495325fedbfe8355e
Max <133808642+max-mejia-13@users.noreply.github.com>
2023-06-02 15:36:35 -0400

    Add files via upload

---
+++ player.py
@@ -0,0 +1,43 @@
+# Team members: Susanne Santos Erenst, Sienna Gaita-Monjaraz, Maximo Mejia
+# CAP4630 - Intro to AI
+# Project 1 - Tic Tac Toe
+# Due: 06/07/2023
+
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+    
+    
+    # we want all players to get their next move given a name
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
